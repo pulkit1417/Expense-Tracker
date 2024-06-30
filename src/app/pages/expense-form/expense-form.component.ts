e978bdf3a631b997de4a0f1d99d15ec8944e9f58
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-06-30 13:15:10 +0530

    form done

--- src/app/pages/expense-form/expense-form.component.ts
+++ src/app/pages/expense-form/expense-form.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-expense-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './expense-form.component.html',
   styleUrl: './expense-form.component.css'
 })
 export class ExpenseFormComponent {
+  expenseForm!:FormGroup;
 
+  constructor(private fb: FormBuilder){
+    this.expenseForm = this.fb.group({
+      price: new FormControl('',[Validators.required]),
+      title: new FormControl('',[Validators.required]),
+      date: new FormControl('',[Validators.required]),
+      description: new FormControl('')
+    })
+  }
+
+  onSubmit(){
+    if(this.expenseForm.valid){
+      console.log(this.expenseForm.value);
+    }else{
+      this.expenseForm.markAllAsTouched();
+    }
+  }
 }
