35f5821265ff1f3da7cf5dd270d165f3f89014e5
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-09 00:33:41 +0530

    env added

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAR_WSqtjLFAeGWqAVdZRPFH0wo5-bPC4M',
-  authDomain: 'expense-tracker-ac96a.firebaseapp.com',
-  databaseURL: 'https://expense-tracker-ac96a-default-rtdb.firebaseio.com',
-  projectId: 'expense-tracker-ac96a',
-  storageBucket: 'expense-tracker-ac96a.appspot.com',
-  messagingSenderId: '615608891151',
-  appId: '1:615608891151:web:1a010d84fdf46fc1f9f07a',
+  apiKey: '${API_KEY}',
+  authDomain: '${AUTH_DOMAIN}',
+  databaseURL: '${DATABASE_URL}',
+  projectId: '${PROJECT_ID}',
+  storageBucket: '${STORAGE_BUCKET}',
+  messagingSenderId: '${MESSAGING_SENDER_ID}',
+  appId: '${APP_ID}',
 };
