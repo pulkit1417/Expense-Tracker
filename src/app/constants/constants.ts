b9bab82a846d816dd1818b5b013f2791f12e3719
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-09 00:47:19 +0530

    database-url added

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
   apiKey: '${API_KEY}',
   authDomain: '${AUTH_DOMAIN}',
-  databaseURL: '${DATABASE_URL}',
+  databaseURL: 'https://expense-tracker-ac96a-default-rtdb.firebaseio.com',
   projectId: '${PROJECT_ID}',
   storageBucket: '${STORAGE_BUCKET}',
   messagingSenderId: '${MESSAGING_SENDER_ID}',
-  appId: '${APP_ID}',
+  appId: '${APP_ID}'
+  
 };
